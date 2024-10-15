2b3bc14421fc06189cc3104963cef70ba830ad4e
Neesh <avaneeshsamsi@gmail.com>
2024-10-15 16:41:38 +0200

    feat: Boilerplate cleanup, add prettier tailwind plugin

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h1>Golden Apple</h1>
+        <div>
+          <p>Museu Del Disseny</p>
+          <p>Barcelona, Spain</p>
+        </div>
+      </div>
+    </nav>
+  )
+}
